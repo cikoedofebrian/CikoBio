b63776faf1183c1499babe620e1c24e7cc29296f
Ciko Edo Febrian <94286488+cikoedofebrian@users.noreply.github.com>
2023-05-03 18:42:45 +0700

    init

--- CikoProfile/ContentView.swift
+++ CikoProfile/ContentView.swift
@@ -9,18 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color
+                .yellow
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                
+                Image("photo").resizable().aspectRatio(contentMode: .fit)
+                    .frame(width: 160, height: 160).clipShape(RoundedRectangle(cornerRadius: 35))
+                    .overlay(RoundedRectangle(cornerRadius: 35)
+                        .stroke(.black, lineWidth: 4)
+                )
+                Text("Ciko Edo Febrian")
+                    .foregroundColor(.black)
+                    .fontWeight(.bold)
+                    .font(Font.custom("Quicksand-Regular", size: 35)).padding(.top, 40)
+                Text("Mobile Developer")
+                    .foregroundColor(.black)
+                    .fontWeight(.bold)
+                    .font(Font.custom("Quicksand-Regular", size: 20))
+                BioItem(content: "Indonesia", icon: "location.fill")
+                BioItem(content: "+6285 155 369 699", icon: "phone.fill")
+                BioItem(content: "ciko.edo.febrians@gmail.com", icon: "envelope.fill")
+
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct BioItem: View {
+    let content: String
+    let icon: String
+    var body: some View {
+        RoundedRectangle(cornerRadius: 35).frame(height: 50)
+            .overlay(HStack {
+                Image(systemName:icon).foregroundColor(.white)
+                Text(content)
+                    .foregroundColor(Color.white)
+                    .font(Font
+                        .custom("Quicksand-Regular", size: 18))
+            })
+            .padding(.top, 5)
+            .padding(.trailing, 10)
+            .padding(.leading, 10)
+     
     }
 }
