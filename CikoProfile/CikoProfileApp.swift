ef3b78dd94dfb4cfbc7b270796b6ae11ed337aa0
Ciko Edo Febrian <94286488+cikoedofebrian@users.noreply.github.com>
2023-05-03 17:29:14 +0700

    Initial Commit

---
+++ CikoProfile/CikoProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  CikoProfileApp.swift
+//  CikoProfile
+//
+//  Created by Ciko Edo Febrian on 03/05/23.
+//
+
+import SwiftUI
+
+@main
+struct CikoProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
